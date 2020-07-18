3670b216e8f789db7d2129762a1898a24bf3ac80
Jennifer Felton <jfelton@csu.fullerton.edu>
2020-07-17 17:38:14 -0700

    Ball Script Created

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+    [SerializeField] float forceValue = 4.5f;
+    private int force = 50;
+    Rigidbody2D myBody;
+    
+
+
+    // Start is called before the first frame update
+    void Start() {
+
+        myBody = GetComponent<Rigidbody2D>();
+        myBody.AddForce(new Vector2(forceValue * force, force));
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
