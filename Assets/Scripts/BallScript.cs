06cebc41824da2bc11038cdcb8c09e1da3d8378d
Jennifer Felton <jfelton@csu.fullerton.edu>
2020-07-18 01:08:13 -0700

    Finished Main Pong Game

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class BallScript : MonoBehaviour {
 
     [SerializeField] float forceValue = 4.5f;
+    [SerializeField] Transform startPoint;
+    const int startForce = 50;
+    const string player1Wall = "Player1Wall";
+    const string player2Wall = "Player2Wall";
     private int force = 50;
     Rigidbody2D myBody;
-    
+    private GameManager gm;
+
 
 
     // Start is called before the first frame update
     void Start() {
 
         myBody = GetComponent<Rigidbody2D>();
-        myBody.AddForce(new Vector2(forceValue * force, force));
+        ResetBall();
+        gm = FindObjectOfType<GameManager>();
 
     }
 
@@ -22,4 +28,28 @@ public class BallScript : MonoBehaviour {
     void Update() {
 
     }
+
+    private void ResetBall() {
+        transform.position = startPoint.position;
+        myBody.velocity = Vector2.zero;
+        myBody.AddForce(new Vector2(forceValue * startForce, startForce));
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == player1Wall) {
+            gm.AddToPlayerScore(2);
+            ResetBall();
+        }
+        if (collision.gameObject.tag == player2Wall) {
+            gm.AddToPlayerScore(1);
+            ResetBall();
+        }
+
+    }
+
+
+
+
+
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField] TMP_Text player1ScoreUI;
+    [SerializeField] TMP_Text player2ScoreUI;
+    private int player1Score = 0;
+    private int player2Score = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void AddToPlayerScore(int playerNumber) {
+        if (playerNumber == 1) {
+            player1Score++;
+            player1ScoreUI.text = $"{player1Score}";
+        }
+        if (playerNumber == 2) {
+            player2Score++;
+            player2ScoreUI.text = $"{player2Score}";
+        }
+
+    }
+}
