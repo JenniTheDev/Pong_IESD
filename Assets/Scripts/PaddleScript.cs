31eef4e79cf38bc0ebee1af159c2d9c6f078ade7
Jennifer Felton <jfelton@csu.fullerton.edu>
2020-07-17 23:56:56 -0700

    Basic Scripting Done

--- Assets/Scripts/PaddleScript.cs
+++ Assets/Scripts/PaddleScript.cs
@@ -7,12 +7,18 @@ public class PaddleScript : MonoBehaviour {
     [SerializeField] bool isPlayerTwo;
     [SerializeField] float speed = 0.2f;
     Transform myTransform;
+    private int direction = 0; // 0 = still, 1 = up, -1 = down
+    private float previousPositionY;
+    private int adjustForce = 5;
+    private int maxY = 4;
+    private int minY = -4;
 
 
 
     // Start is called before the first frame update
     void Start() {
         myTransform = transform;
+        previousPositionY = myTransform.position.y;
     }
 
     // Update is called once per frame
@@ -30,6 +36,10 @@ public class PaddleScript : MonoBehaviour {
                 MoveDown();
         }
 
+        Vector2 pos = myTransform.position;
+        pos.y = Mathf.Clamp(myTransform.position.y, minY, maxY);
+        myTransform.position = pos;
+
     }
 
     void MoveUp() {
@@ -38,6 +48,24 @@ public class PaddleScript : MonoBehaviour {
 
     void MoveDown() {
         myTransform.position = new Vector2(myTransform.position.x, myTransform.position.y - speed);
+    }
+
+    private void LateUpdate() {
+        previousPositionY = myTransform.position.y;
+
+        if (previousPositionY > myTransform.position.y)
+            direction = -1;
+        else if (previousPositionY < myTransform.position.y)
+            direction = 1;
+        else
+            direction = 0;
+
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+        float adjust = adjustForce * direction;
+        collision.rigidbody.velocity = new Vector2(collision.rigidbody.velocity.x, collision.rigidbody.velocity.y + adjust);
     }
 
 
