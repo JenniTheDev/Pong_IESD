d5b5bbf6454cfe7913a5d4600bae24ae641b7d60
Vast <Vastlee@gmail.com>
2020-07-23 12:05:53 -0700

    Moved EventController to static Singleton

--- Assets/Scripts/EventController.cs
+++ Assets/Scripts/EventController.cs
@@ -1,34 +1,19 @@
 ﻿using UnityEngine;
 
-public class EventController : MonoBehaviour {
+public class EventController {
     #region Singleton
-    private static bool isQuitting;
+    private static bool isQuitting = false;
     private static EventController instance = null;
     public static EventController Instance {
         get {
             if(instance == null && !isQuitting) {
-                FindOrCreateInstance();
+                instance = new EventController();
                 Application.quitting += () => isQuitting = true;
             }
             return instance;
         }
     }
 
-    /// <summary>Looks for an existing instance, if not found creates one. If multiple are found, reports error.</summary>
-    private static void FindOrCreateInstance() {
-        EventController[] instanceArray = FindObjectsOfType<EventController>();
-        if(instanceArray.Length == 0) {
-            GameObject singleton = new GameObject(typeof(EventController).Name);
-            instance = singleton.AddComponent<EventController>();
-            DontDestroyOnLoad(singleton);
-        } else if(instanceArray.Length == 1) {
-            instance = instanceArray[0];
-            DontDestroyOnLoad(instance);
-        } else if(instanceArray.Length > 1) {
-            Debug.LogError("<color=yellow>Multiple instances of the singleton [" + typeof(EventController).Name + "] exists.</color>");
-            Debug.Break();
-        }
-    }
     #endregion
 
     #region Events & Delegates
