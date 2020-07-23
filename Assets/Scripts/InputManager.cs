65f13f98f34a1615b14739ae0aa4fd6ea5897783
Vast <Vastlee@gmail.com>
2020-07-23 09:27:43 -0700

    Setup Input Manager, Adjusted settings on Paddles

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+    [SerializeField] private Paddle p1Paddle;
+    [SerializeField] private Paddle p2Paddle;
+
+    [SerializeField] private KeyCode p1UpKey;
+    [SerializeField] private KeyCode p1DownKey;
+    [SerializeField] private KeyCode p2UpKey;
+    [SerializeField] private KeyCode p2DownKey;
+
+    private IMoveablePaddle p1Input;
+    private IMoveablePaddle p2Input;
+
+    #region MonoBehaviour
+    private void Start() {
+        p1Input = p1Paddle.GetComponent<IMoveablePaddle>();
+        p2Input = p2Paddle.GetComponent<IMoveablePaddle>();
+    }
+
+    private void Update() {
+        if(Input.GetKey(p1UpKey)) {
+            p1Input.MoveUp();
+        }
+        if(Input.GetKey(p1DownKey)) {
+            p1Input.MoveDown();
+        }
+        if(Input.GetKey(p2UpKey)) {
+            p2Input.MoveUp();
+        }
+        if(Input.GetKey(p2DownKey)) {
+            p2Input.MoveDown();
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Paddle : MonoBehaviour, IMoveablePaddle {
+    [SerializeField] private float moveForce;
+
+    private Rigidbody2D rb;
+
+    #region MonoBehaviour
+
+    void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    #endregion
+
+    #region Methods
+    public void MoveUp() {
+        MovePaddle(PaddleDirection.Up);
+    }
+
+    public void MoveDown() {
+        MovePaddle(PaddleDirection.Down);
+    }
+
+    private void MovePaddle(PaddleDirection dir) {
+        var moveTowards = Vector2.up * ((dir == PaddleDirection.Up) ? moveForce : -moveForce);
+        rb.AddForce(moveTowards);
+    }
+    #endregion
+}
